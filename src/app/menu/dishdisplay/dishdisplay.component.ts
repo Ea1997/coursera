c80b07511712c4df6ee0a5a1e6ed8b3b9047fa65
Elazhari Salim <salim.elazhari@gmail.com>
2020-08-14 17:26:05 +0100

    assignement 1

--- src/app/menu/dishdisplay/dishdisplay.component.ts
+++ src/app/menu/dishdisplay/dishdisplay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dishdisplay',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dishdisplay.component.css']
 })
 export class DishdisplayComponent implements OnInit {
-
+  @Input() data:any;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.data)
   }
 
 }
