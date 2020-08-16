df245677eb2ff0747f1e200a8f29d6829702a2e9
Elazhari Salim <salim.elazhari@gmail.com>
2020-08-16 19:56:34 +0100

    Template reactive form 1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {Feedback , ContactType} from '../menu/shared/feedback';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  feedbackForm:FormGroup;
+feedback:Feedback;
+contactType=ContactType;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) { 
+    this.createFrom();
+  }
 
   ngOnInit(): void {
+  }
+  createFrom(){
+    this.feedbackForm=this.fb.group({
+      firstname:'',
+      lastname:'',
+      telnum:0,
+      email:'',
+      agree:false,
+      contacttype:'None',
+      message:''
+    })
+  }
+  onSubmit(){
+    this.feedback=this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
